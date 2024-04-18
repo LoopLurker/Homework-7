8253132db3fed76718355f06eda38ae2e2549822
LoopLurker <ily.gogichaishvili@gmail.com>
2024-04-18 23:15:35 +0400

    switch changed with if else

--- java.js
+++ java.js
@@ -6,22 +6,17 @@ let correctAnswer;
 
 let choice = Number(window.prompt(prompt));
 
-switch (choice) {
-  case 1:
-    question = "რა არის რა არის typeof(5)?";
-    correctAnswer = "number";
-    break;
-  case 2:
-    question = "შეიძლება თუ არა let-ის რედეკლალირება";
-    correctAnswer = "Yes";
-    break;
-  case 3:
-    question = "რას აბრუნებს console.log(Math.floor(2.1))";
-    correctAnswer = "2";
-    break;
-  default:
-    console.log("Invalid choice");
-    break;
+if (choice === 1) {
+  question = "რა არის რა არის typeof(5)?";
+  correctAnswer = "number";
+} else if (choice === 2) {
+  question = "შეიძლება თუ არა let-ის რედეკლალირება";
+  correctAnswer = "Yes";
+} else if (choice === 3) {
+  question = "რას აბრუნებს console.log(Math.floor(2.1))";
+  correctAnswer = "2";
+} else {
+  console.log("Invalid choice");
 }
 
 let userAnswer = window.prompt(question);
